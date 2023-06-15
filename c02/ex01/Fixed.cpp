e66814467692dd71d5c3567485dc0b312a73aed1
saadivar <115896775+saadivar@users.noreply.github.com>
2023-06-16 00:39:44 +0100

    Add files via upload

---
+++ c02/ex01/Fixed.cpp
@@ -0,0 +1,53 @@
+#include "Fixed.hpp"
+
+    Fixed::Fixed()
+    {
+        std::cout <<"Default constructor called"<<std::endl;
+        this->fixed_point = 0;
+    }
+    Fixed::Fixed(const int frac) : fixed_point(frac<<num_fra)
+    {
+        std::cout <<"int constructor called"<<std::endl;
+    }
+    Fixed::Fixed(const float f_p) : fixed_point(roundf((f_p * (1 << num_fra))))
+    {
+        std::cout <<"Float constructor called"<<std::endl;
+    }
+    Fixed::Fixed(const Fixed & other)
+    {
+        std::cout <<"Copy constructor called"<<std::endl;
+        *this = other;
+    }
+    Fixed& Fixed::operator=(const Fixed& other)
+    {
+        std::cout <<"Copy assignment operator called"<<std::endl;
+        this->fixed_point = other.getRawBits();
+        return(*this);
+    }
+        Fixed::~Fixed()
+        {
+            std::cout <<"Destructor called"<<std::endl;
+        }
+    int Fixed::getRawBits()const
+    {
+        return this->fixed_point;
+    }
+    
+     void Fixed::setRawBits( int const raw )
+     {
+        this->fixed_point = raw;
+     }
+
+     float Fixed::toFloat( void ) const
+     {
+        float scale = 1 << num_fra;
+        return this->fixed_point / scale;
+     }
+      int Fixed::toInt( void ) const
+     {
+        return (this->fixed_point >> num_fra);
+     }
+     std::ostream & operator<<(std::ostream & stream, Fixed const & other) {
+	    stream << other.toFloat();
+	return stream;
+    }
