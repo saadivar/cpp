4484e262e3a23a1192ab6bb6b325c1ebd7a497a9
saadivar <115896775+saadivar@users.noreply.github.com>
2023-06-22 00:50:22 +0100

    Delete mo04 directory

--- mo04/ex03/IMatreriaSource.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once 
-
-class IMateriaSource
-{
-    public:
-    virtual ~IMateriaSource() {}
-    virtual void learnMateria(AMateria*) = 0;
-    virtual AMateria* createMateria(std::string const & type) = 0;
-};
