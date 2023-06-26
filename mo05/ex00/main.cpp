97d033c4f6decab45fb8df3c07b95c1aeac6e868
saadivar <115896775+saadivar@users.noreply.github.com>
2023-06-26 21:44:09 +0100

    Add files via upload

---
+++ mo05/ex00/main.cpp
@@ -0,0 +1,21 @@
+#include "Bureaucrat.hpp"
+
+
+int main( void )
+{
+    try {
+        Bureaucrat bureaucrat("saad", 1);
+		bureaucrat.increment();
+        std::cout << bureaucrat << std::endl;
+    } catch (Bureaucrat::GradeTooHighException &e) {
+        std::cout << e.what() << std::endl;
+    }
+	try {
+        Bureaucrat bureaucrat("adam", 150);
+		bureaucrat.decrement();
+        std::cout << bureaucrat << std::endl;
+    } catch (Bureaucrat::GradeTooLowException &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
